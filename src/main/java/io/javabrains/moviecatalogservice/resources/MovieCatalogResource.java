7f22afe140c06fd3dbd2762318bf4e8b598255a4
shreyachatterjee2111 <33459898+shreyachatterjee2111@users.noreply.github.com>
2019-04-07 02:39:04 +0530

    Uploading Local Changes

---
+++ src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,20 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+            return Collections.singletonList(
+                    new CatalogItem("Transfomer","Movie about Autobots and Decepticons",4)
+            );
+    }
+}
